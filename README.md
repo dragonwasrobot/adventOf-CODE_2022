2f3b5e5f4acda7408a863f0b66444b4d60664944
Peter Urbak <peter@dragonwasrobot.com>
2022-12-04 16:14:55 +0100

    Adds solution to day 4 in SQL/SQLite

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ when you can choose to hack your way through it in 25 different languages.
 - Day 1: [Prolog](https://www.swi-prolog.org/)
 - Day 2: [Bash](https://www.gnu.org/software/bash/)
 - Day 3: [Pharo](https://pharo.org/)
+- Day 4: [SQL/SQlite](https://www.sqlite.org/index.html)
---
+++ day4-solution.sql
@@ -0,0 +1,34 @@
+-- $ sqlite3 advent-of-code.db
+CREATE TABLE day4 (
+  left_elf VARCHAR(50) NOT NULL,
+  right_elf VARCHAR(50) NOT NULL
+);
+
+.separator ","
+.import "day4-input.txt" day4
+
+ALTER TABLE day4 ADD COLUMN left_elf_start INTEGER;
+ALTER TABLE day4 ADD COLUMN left_elf_stop INTEGER;
+ALTER TABLE day4 ADD COLUMN right_elf_start INTEGER;
+ALTER TABLE day4 ADD COLUMN right_elf_stop INTEGER;
+
+UPDATE day4 SET left_elf_start = SUBSTR(left_elf, 1, INSTR(left_elf, '-') - 1);
+UPDATE day4 SET left_elf_stop = SUBSTR(left_elf, INSTR(left_el, '-') + 1);
+UPDATE day4 SET right_elf_start = SUBSTR(right_elf, 1, INSTR(right_elf, '-') - 1);
+UPDATE day4 SET right_elf_stop = SUBSTR(right_elf, INSTR(right_elf, '-') + 1);
+
+-- Solution 1
+SELECT COUNT(*)
+  FROM day4
+  WHERE (left_elf_start >= right_elf_start AND
+         left_elf_stop <= right_elf_stop)
+  OR (right_elf_start >= left_elf_start AND
+      right_elf_stop <= left_elf_stop); -- 500
+
+-- Solution 2
+SELECT COUNT(*)
+  FROM day4
+  WHERE (left_elf_start <= right_elf_start AND
+         left_elf_stop >= right_elf_start)
+    OR (left_elf_start >= right_elf_start AND
+        left_elf_start <= right_elf_stop); -- 815
